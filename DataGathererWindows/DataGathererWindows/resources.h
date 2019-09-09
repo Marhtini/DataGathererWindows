4ae8980d1ac9505b38f4e3f79e584e866492aa76
Marhtini <Marhtini@users.noreply.github.com>
2019-09-09 11:20:17 -0400

    Update resources.h
    Refactored for simplicity, removed Vector of Vectors and now using individual string vectors to store system information.

--- DataGathererWindows/DataGathererWindows/resources.h
+++ DataGathererWindows/DataGathererWindows/resources.h
@@ -11,12 +11,12 @@
 #pragma comment(lib, "Iphlpapi.lib")
 
 //Declaration of C++ Functions in DataGathererWindows.cpp
-void setCIComputerData(std::vector<std::vector<std::string>> pMasterCIDataVector);
-std::vector<std::string> getCIHardwareData(std::vector<std::vector<std::string>> pMasterCIDataVector);
-std::vector<std::string> getCIComputerData(std::vector<std::vector<std::string>> pMasterCIDataVector);
-std::vector<std::string> getCIWinVersionData(std::vector<std::vector<std::string>> pMasterCIDataVector);
-std::vector<std::string> getCIWinServerData(std::vector<std::vector<std::string>> pMasterCIDataVector);
-std::vector<std::string> getCIDiskData(std::vector<std::vector<std::string>> pMasterCIDataVector);
-std::vector<std::string> getCIProcessData(std::vector<std::vector<std::string>> pMasterCIDataVector);
-std::vector<std::string> getCINetworkData(std::vector<std::vector<std::string>> pMasterCIDataVector);
-std::vector<std::string> getCIModelData(std::vector<std::vector<std::string>> pMasterCIDataVector);
+void setCIComputerData();
+std::vector<std::string> getCIHardwareData(std::vector<std::string> pMasterCIHardwareDataVector);
+std::vector<std::string> getCIComputerData(std::vector<std::string> pMasterCIComputerDataVector);
+std::vector<std::string> getCIWinVersionData(std::vector<std::string> pMasterCIWinVersionDataVector);
+std::vector<std::string> getCIWinServerData(std::vector<std::string> pMasterCIWinServerDataVector);
+std::vector<std::string> getCIDiskData(std::vector<std::string> pMasterCIDiskDataVector);
+std::vector<std::string> getCIProcessData(std::vector<std::string> pMasterCIProcessDataVector);
+std::vector<std::string> getCINetworkData(std::vector<std::string> pMasterCINetworkDataVector);
+std::vector<std::string> getCIModelData(std::vector<std::string> pMasterCIModelDataVector);
